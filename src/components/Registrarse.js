694242f9e46711c2e122d9c05331b2d005d5f853
SINIS <juan_seb.sinisterra@uao.edu.co>
2021-05-23 15:02:45 -0500

    Finalización del componente Registrarse

--- src/components/Registrarse.js
+++ src/components/Registrarse.js
@@ -11,43 +11,43 @@ function Registrarse() {
                 <img className="o-logo" id="logoazul" src={logo} alt="logo" />
                 <h2 className="o-registertext">Regístrate</h2>
                 <div className="o-text">
-                    <h3 className="o-mensaje">La casa de tu sueños te está esperando. :)</h3>
+                    <p className="o-mensaje">La casa de tu sueños te está esperando. :)</p>
                     <a className="o-crearcuentaempresa" href="#">Crea una cuenta de empresa</a>
                 </div>
                 <form className="o-formulario" action="#">
                     <div className="o-camposizquierdos">
-                    <p>Nombre</p>
+                    <p className="s-etiqueta" >Nombre</p>
                     <input type="text" className="o-campo" />
                     </div>
                     <div className="o-camposderechos">
-                    <p>Apellido</p>
+                    <p  className="s-etiqueta">Apellido</p>
                     <input type="text" className="o-campo" />
                     </div>
                     <div className="o-camposizquierdos">
-                    <p>Número de télefono</p>
+                    <p  className="s-etiqueta">Número de télefono</p>
                     <input type="number" className="o-campo" />
                     </div>
                     <div className="o-camposderechos">
-                    <p>Correo electrónico</p>
+                    <p  className="s-etiqueta" >Correo electrónico</p>
                     <input type="text" className="o-campo" />
                     </div>
                     <div className="o-camposizquierdos">
-                    <p>Contraseña</p>
+                    <p  className="s-etiqueta" >Contraseña</p>
                     <input type="password" className="o-campo" />
                     </div>
                     <div className="o-camposderechos">
-                    <p>Confirmar contraseña</p>
+                    <p  className="s-etiqueta" >Confirmar contraseña</p>
                     <input type="password" className="o-campo" />  
                     </div>                
                 </form>
                 <div className="o-terminosycondiciones">
                     <input type="checkbox" id="terminos" />
-                    <label htmlFor="terminos">Acepto los <a href="#">Términos y Condiciones.</a></label>
+                    <label htmlFor="terminos">Acepto los <a className="s-vinculo" href="#">Términos y Condiciones.</a></label>
                 </div>
                 <div className="o-crearcuenta">
                     <button className="o-botoncrearcuenta" type="button" name="crear cuenta">Crear cuenta</button>
                 </div>
-                <h3 className="o-tenercuenta">¿Ya tienes una cuenta? <a href="#">Inicia sesión</a></h3><a href="#">
+                <p className="o-tenercuenta">¿Ya tienes una cuenta? <a className="s-vinculo" href="#">Inicia sesión</a></p><a href="#">
                 </a></div><a href="#">
                 </a></div><a href="#"></a>
             </div>
