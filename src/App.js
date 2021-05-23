2a4e1da9d98ae8b33d9d6c611023001ef1742c1b
Juan Esteban <juan_esteban.garcia@uao.edu.co>
2021-05-23 18:58:05 -0500

    rutas

--- src/App.js
+++ src/App.js
@@ -3,12 +3,21 @@ import Login from './components/Login'
 import ResContraseña from './components/ResContraseña';
 import Registrarse from './components/Registrarse';
 import Home from './components/Home';
+import {BrowserRouter,Switch ,Route} from 'react-router-dom';
 
 
 function App() {
   return (
     <div className="App">      
-      <Registrarse></Registrarse>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/login" component={Login}/>
+          <Route path="/home" component={Home}/>
+          <Route path="/restaurarcontraseña" component={ResContraseña}/>
+          <Route path="/regristrarse" component={Registrarse}/>
+        </Switch>
+      </BrowserRouter>
+
     </div>
   );
 }
