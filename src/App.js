e48f86fbec7f5803d591ecd26d4aeefd4253ccd8
SINIS <juan_seb.sinisterra@uao.edu.co>
2021-05-23 10:40:29 -0500

    Creacion del componente restablecer contraseña

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
 import Login from './components/Login'
+import ResContraseña from './components/ResContraseña';
+
 
 function App() {
   return (
-    <div className="App">
-      
+    <div className="App">      
       <Login></Login>
     </div>
   );
